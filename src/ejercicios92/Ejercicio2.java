f2281aa5d0658e98708bc26e7502b7c3338c7cfd
Mario Gutierrez <169930559+Mario9924@users.noreply.github.com>
2026-05-15 23:43:54 +0200

    Se añade función 'obtenerInformacion' que permite volcar la información de l fichero en un HashMap para su uso posterior por el resto de funciones necesarias

--- src/ejercicios92/Ejercicio2.java
+++ src/ejercicios92/Ejercicio2.java
@@ -2,31 +2,43 @@ package ejercicios92;
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Scanner;
 
 /**
- *
- * @author Mario Guti�rrez
+ * 2) Desarrollar un programa nuevo para gestionar recetas de cocina en un
+ * fichero. Debe permitir: 
+ * • Añadir nuevas recetas al archivo (no se puede introducir otra con un nombre que ya exista, idem al ejercicio anterior...).
+ * • Listar todas las guardadas actualmente. 
+ * • Buscar todas las recetas con un ingrediente. 
+ * 
+ * Cada receta puede tener asociada una lista de ingredientes y pasos para la preparación. 
+ * El formato del archivo será el que sigue:
+ * NOMBRE_RECETA | INGREDIENTE1 ; INGREDIENTE2 ;... ; INGREDIENTE N | PASO1;
+ * PASO 2; ... ; PASO M.
+ * 
+ * 
+ * @author Mario Gutiérrez
  * @see https://classroom.google.com/c/ODA3NDY5OTY5MTcz/a/ODYyOTYzMjczMzkw/details
  * @version 1.0
  */
 public class Ejercicio2 {
     public static void main(String[] args) {
-        // Declaraci�n de variables
+        // Declaración de variables
         Scanner reader = new Scanner(System.in);
-        HashSet<String> listadoRecetas = new HashSet<>();
-        File fichero = new File(".\\src\\ficheros\\ejercicio2.txt");
+        HashSet<String> nombresRecetas = new HashSet<>();
+        File fichero = new File(".\\src\\ficheros\\recetas.txt");
         if (fichero.exists()){
             System.out.println("El fichero existe");
             try {
                 Scanner rf = new Scanner(fichero);
                 while (rf.hasNext()){
                     String[] informacion = rf.nextLine().split("\\|");
-                    listadoRecetas.add(informacion[0]);
+                    nombresRecetas.add(informacion[0]);
                 }
                 rf.close();
-                System.out.println(listadoRecetas);
+                System.out.println(nombresRecetas);
             } catch (FileNotFoundException ex) {
                 System.getLogger(Ejercicio2.class.getName()).log(System.Logger.Level.ERROR, (String) null, ex);
             }
@@ -42,7 +54,7 @@ public class Ejercicio2 {
         while (true){
             System.out.println("Dime el nombre de la receta que quieres incluir: ");
             nombreReceta = reader.nextLine();
-            if (listadoRecetas.contains(nombreReceta) || listadoRecetas.contains(nombreReceta.replace(" ", "")) || listadoRecetas.contains(nombreReceta.toLowerCase().replace(" ", ""))){
+            if (nombresRecetas.contains(nombreReceta) || nombresRecetas.contains(nombreReceta.replace(" ", "")) || nombresRecetas.contains(nombreReceta.toLowerCase().replace(" ", ""))){
                 System.out.println("El nombre elegido ya existe, elige otro por favor");
             } else {
                 break;
@@ -50,6 +62,41 @@ public class Ejercicio2 {
             }
         }
         
-        System.out.println("Has elegido el nomre: " + nombreReceta);
+        System.out.println("Has elegido el nombre: " + nombreReceta);
+    }
+    
+    
+    /**
+     * Esta función permite almacenar toda la información disponible del fichero de recetas en un HashMap
+     *  El formato es siempre el mismo, Key = Nombre receta, Value = Ingredientes y pasos
+     */
+    public static HashMap<String, String> obtenerInformacion(File ficheroRecetas){
+        HashMap<String,String> recetas = new HashMap<>();
+        try {
+            //Creamos un flujo de datos para obtener la información del fichero
+            Scanner rf = new Scanner(ficheroRecetas);
+            while(rf.hasNext()){
+                String[] informacion = rf.nextLine().split("\\|");
+                // Almacenamos los datos de las recetas en un hashmap
+                recetas.put(informacion[0], informacion[1]+informacion[2]);
+            }
+            rf.close();
+        } catch (FileNotFoundException fnfex) {
+            System.err.println("Error a la hora de trabajar con el fichero: " + fnfex);
+        }
+        return recetas;
+    }
+    
+    public static void addRecipe(){
+        
+    }
+    
+    public static void mostrarRecetas(){
+        
+    }
+    
+    
+    public static void buscarIngrediente(String ingrediente){
+        
     }
 }
