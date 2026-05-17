29b6ae57a641393c77fde30e3371519deaf6e4d1
Mario Gutierrez <169930559+Mario9924@users.noreply.github.com>
2026-05-17 17:34:32 +0200

    Se corrige la función de 'hacerLlamada' para eliminar correctamente el elemento que supera la longitud máxima del listado. Anteriormente se realizaba el borrado pero no se añadía nada, se modifica para que en caso de llegar a la longitud primero se borre la entrada y luego se añada C

--- src/ejercicio3/Ejercicio3.java
+++ src/ejercicio3/Ejercicio3.java
@@ -1,4 +1,5 @@
 package ejercicio3;
+import java.io.File;
 import java.util.Scanner;
 
 /**
@@ -16,7 +17,37 @@ public class Ejercicio3 {
      */
     public static void main(String[] args) {
         // Declaración de variables
+        File ficheroConfiguracion = new File("");
         Scanner reader = new Scanner(System.in);
+        
+        SmartPhone miMovil = new SmartPhone("Negro");
+        
+        /*
+            Hacemos un par de pruebas de funcionamiento
+        */
+        
+        miMovil.mostrarLlamadas(); // No mostrará nada ya que no hay llamadas por el momento
+        
+        miMovil.hacerLlamada("Antonio", "Entrante");
+        miMovil.hacerLlamada("Maria", "Saliente");
+        miMovil.hacerLlamada("Lola", "Saliente");
+        
+        miMovil.mostrarLlamadas(); // Mostrará las llamadas anteriores
+        
+        System.out.println("");
+        // Hacemos 2 más
+        
+        miMovil.hacerLlamada("Pepe", "Entrante");
+        miMovil.hacerLlamada("Laura", "Entrante");
+        
+        miMovil.mostrarLlamadas();
+        
+        // Si ahora añadimos una más, quitará la primera entrada que encuentre
+        System.out.println("Patata");
+        miMovil.hacerLlamada("Chelo");
+        miMovil.mostrarLlamadas();
+        
+        
     }
     
 }
--- src/ejercicio3/SmartPhone.java
+++ src/ejercicio3/SmartPhone.java
@@ -17,62 +17,60 @@ import java.util.Scanner;
  * https://classroom.google.com/c/ODA3NDY5OTY5MTcz/a/ODYyOTYzMjczMzkw/details
  * @version 1.0
  */
-  
+public class SmartPhone implements Serializable {
 
-    public class SmartPhone implements Serializable {
+    private transient static Scanner reader = new Scanner(System.in);
+    private transient String color;
+    private transient final int precio = 1100;
+    private transient final String marca = "πj-iphone";
+    private String imei;
+    private int brillo;
+    private int volumen;
+    private HashMap<String, String> listadoLlamadas;
 
-        private transient static Scanner reader = new Scanner(System.in);
-        private transient String color;
-        private transient final int precio = 1100;
-        private transient final String marca = "πj-iphone";
-        private String imei;
-        private int brillo;
-        private int volumen;
-        private HashMap<String, String> listadoLlamadas;
+    public SmartPhone(String colorIn) {
+        this.color = colorIn;
+        this.brillo = (int) Math.floor(Math.random() * 11);
+        this.volumen = (int) Math.floor(Math.random() * 101);
+        this.imei = setImei();
+        this.listadoLlamadas = new HashMap<>(); // Si no se inicializa siempre será null
+    }
 
-        public SmartPhone(String colorIn) {
-            this.color = colorIn;
-            this.brillo = (int) Math.floor(Math.random() * 11);
-            this.volumen = (int) Math.floor(Math.random() * 101);
-            this.imei = setImei();
-        }
+    private HashMap<String, String> getListado() {
+        return this.listadoLlamadas;
+    }
 
-        private HashMap<String, String> getListado() {
-            return this.listadoLlamadas;
-        }
+    public String getImei() {
+        return this.imei;
+    }
 
-        public String getImei() {
-            return this.imei;
-        }
-
-        private String setImei() {
-            int numeroAleatorio = 0; //+65
-            String imeiGenerado = "";
-            for (int i = 0; i < 18; i++) {
-                numeroAleatorio = (int) Math.floor((Math.random() * 26) + 65);
-                if (i == 6 || i == 9 || i == 16) {
-                    imeiGenerado += "/";
-                } else {
-                    imeiGenerado += (char) numeroAleatorio;
-                }
-            }
-            return imeiGenerado;
-        }
-
-        public void hacerLlamada(String contactoIn, String tipoLlamada) {
-            if (this.getListado().size() == 5) {
-                System.out.println("Se han realizado 5 llamadas, pasamos a eliminar una de ellas");
-                Iterator<String> iterador = this.getListado().keySet().iterator();
-                while (iterador.hasNext()) {
-                    iterador.next();
-                    iterador.remove();
-                    break;
-                }
-                System.out.println(this.getListado());
+    private String setImei() {
+        int numeroAleatorio = 0; //+65
+        String imeiGenerado = "";
+        for (int i = 0; i < 18; i++) {
+            numeroAleatorio = (int) Math.floor((Math.random() * 26) + 65);
+            if (i == 6 || i == 9 || i == 16) {
+                imeiGenerado += "/";
             } else {
-                this.getListado().put(contactoIn, tipoLlamada);
+                imeiGenerado += (char) numeroAleatorio;
             }
         }
+        return imeiGenerado;
+    }
+
+    public void hacerLlamada(String contactoIn, String tipoLlamada) {
+        if (this.getListado().size() == 5) {
+            System.out.println("Se han realizado 5 llamadas, pasamos a eliminar una de ellas");
+            Iterator<String> iterador = this.getListado().keySet().iterator();
+            while (iterador.hasNext()) {
+                iterador.next();
+                iterador.remove();
+                break;
+            }    
+        }
+        this.getListado().put(contactoIn, tipoLlamada);
+
+    }
 
     public void hacerLlamada(String contactoIn) {
         if (this.getListado().size() == 5) {
@@ -83,25 +81,35 @@ import java.util.Scanner;
                 iterador.remove();
                 break;
             }
-            System.out.println(this.getListado());
-        } else {
-            try {
-                int opcionIn = 0;
-                String tipoLlamada = "Entrante";
-                while (opcionIn < 0 || opcionIn > 1) {
-                    System.out.println("La llamada hacia " + contactoIn + "es :"
+        }
+        int opcionIn = 0;
+            String tipoLlamada = "Entrante";
+            do {
+                try {
+                    System.out.println("La llamada hacia " + contactoIn + " es :"
                             + "\n0-Entrante"
                             + "\n1-Saliente");
                     opcionIn = reader.nextInt();
+                } catch (InputMismatchException ime) {
+                    System.err.println("No se permite el dato introducido: " + ime);
+                    reader.nextLine();
                 }
-                if (opcionIn == 1) {
-                    tipoLlamada = "Saliente";
-                }
-                this.getListado().put(contactoIn, tipoLlamada);
-            } catch (InputMismatchException ime) {
-                System.err.println("No se permite el dato introducido: " + ime);
-                reader.nextLine();
+
+            } while (opcionIn < 0 || opcionIn > 1);
+            if (opcionIn == 1) {
+                tipoLlamada = "Saliente";
+            }
+            this.getListado().put(contactoIn, tipoLlamada);
+    }
+
+    public void mostrarLlamadas() {
+        if (this.getListado() == null) {
+            System.out.println("No se han realizado llamadas por el momento");
+        } else {
+            for (String key : this.getListado().keySet()) {
+                System.out.println(key + " - " + this.getListado().get(key));
             }
         }
     }
+
 }
